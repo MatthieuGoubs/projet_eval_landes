f21c1cde8f4fa90001ab496bdf5d50c0104f1426
matthieugoubert <matthieu.goubert@etu.univ-tlse2.fr>
2026-01-26 01:44:59 +0000

    # Quasi remise au propre du code python (encore 2 blocs)

--- my_function.py
+++ my_function.py
@@ -1,25 +1,43 @@
 
-import geopandas as gpd
+# création de la fonction  'rasterisation'
 
-def rasterisation (my_folder, in_vector, ref_image, out_image, field_name, sptial_resolution, xmin, ymin, xmax, ymax): 
-    my_folder = my_folder
-    in_vector = in_vector
-    ref_image = ref_image
-    out_image = out_image
-    field_name = field_name
+from osgeo import gdal
+import os
 
-    sptial_resolution = sptial_resolution
-    xmin = xmin
-    ymin = ymin
-    xmax = xmax
-    ymax = ymax
-    
-    cmd_pattern = ("gdal_rasterize -a {field_name} "
-               "-tr {sptial_resolution} {sptial_resolution} "
-               "-te {xmin} {ymin} {xmax} {ymax} -ot Byte -of GTiff "
-               "{in_vector} {out_image}")
+def rasterisation(in_vector, ref_image, out_image, field_name,
+                  dtype="Int32", nodata=0, all_touched=True):
 
-    cmd = cmd_pattern.format(in_vector=in_vector, xmin=xmin, ymin=ymin, xmax=xmax, ymax=ymax, out_image=out_image, field_name=field_name, sptial_resolution=sptial_resolution)
+    # --- 1) Lecture des infos spatiales depuis l'image de référence ---
+    ds = gdal.Open(ref_image)
+    gt = ds.GetGeoTransform()
+    xsize = ds.RasterXSize
+    ysize = ds.RasterYSize
 
-    # execute the command in the terminal 
+    # résolution (pixel size)
+    res_x = gt[1]
+    res_y = abs(gt[5])
+
+    # emprise spatiale
+    xmin = gt[0]
+    ymax = gt[3]
+    xmax = xmin + xsize * res_x
+    ymin = ymax - ysize * res_y
+
+    # --- 2) Options de GDAL ---
+    touched_flag = "-at" if all_touched else ""
+    nodata_opt = f"-a_nodata {nodata}" if nodata is not None else ""
+
+    # --- 3) Construction la ligne de commande gdal ---
+    cmd = (
+        f"gdal_rasterize -a {field_name} "
+        f"-tr {res_x} {res_y} "
+        f"-te {xmin} {ymin} {xmax} {ymax} "
+        f"-ot {dtype} -of GTiff {nodata_opt} {touched_flag} "
+        f"{in_vector} {out_image}"
+    )
+
+    print("Commande exécutée :")
+    print(cmd)
+
+    # --- 4) Exécution ---
     os.system(cmd)
